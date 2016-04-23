1712a174989fff09c873a48eea31b59e0faadf6e
Maitland McConnell <maitlandm@gmail.com>
2016-04-23 14:22:04 -0700

    changed app.js file

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var path = require("path");
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname + "/client")));
 
 var fish = require("./fish-scraper.js");
 
